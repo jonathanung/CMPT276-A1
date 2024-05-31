8aee7e504a4ce6f01afd5d591552ebb2d54b24f8
Jonathan Ung <jonathan.keith.ung@gmail.com>
2024-05-31 12:43:07 -0700

    deploy test2

---
+++ src/test/java/com/example/grade-calculator/GradeCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.grade-calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeCalculatorApplication.class, args);
+    }
+}
+
+
